1687dcffc6d30f7774c39ab8b4c9c6ffb1502a73
Andrii Tarasenko <bidstrup2000@gmail.com>
2013-08-23 22:05:54 +0300

    t3_class_based_view

--- LikeTwitter/urls.py
+++ LikeTwitter/urls.py
@@ -1,20 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from LikeTwitter.apps.notes.views import AllNotesView, NoteByIdView 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'LikeTwitter.apps.notes.views.index', name='index'),
-    # url(r'^LikeTwitter/', include('LikeTwitter.foo.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^$',  AllNotesView.as_view(), name='AllNotesView'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'(?:.*?/)?(?P<path>(css|jquery|jscripts|images)/.+)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }), 
-    url(r'^notes/(?P<id_of_note>[0-9]+)', 'LikeTwitter.apps.notes.views.search_id', name='search_id'),    
-    url(r'^notes/$', 'LikeTwitter.apps.notes.views.index', name='index'),
+    url(r'(?:.*?/)?(?P<path>(css|jquery|js|images)/.+)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }), 
+    url(r'^notes/(?P<id_of_note>[0-9]+)', NoteByIdView.as_view() , name='NoteByIdView'),   
+    url(r'^notes/', AllNotesView.as_view(), name='AllNotesView'),
 )
 
