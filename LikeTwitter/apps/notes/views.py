012ef54e663b87e822349693de9d7c8c2dca6a01
Andrii Tarasenko <bidstrup2000@gmail.com>
2013-08-24 18:42:10 +0300

    t5_class_based_view

--- LikeTwitter/apps/notes/views.py
+++ LikeTwitter/apps/notes/views.py
@@ -1,29 +1,56 @@
 from django.shortcuts import render_to_response 
-
-# Create your views here.
 from django.template import loader, Context
 from django.template import RequestContext
 from django.http import HttpResponse
 from LikeTwitter.apps.notes.models import Note
 from LikeTwitter.apps.notes.forms import NewNoteForm
+from django.views.generic.base import View
 
-def index(request):
-    #m = Note.objects.filter(pk__gt=7).delete()
-    notes_list = Note.objects.all()
-    t = loader.get_template(r'notes.html')
-    c = Context({'notes':notes_list})
-    if request.method == "POST":
-        #request with data
+class AllNotesView(View):        
+    """
+    Display an individual :model:`LikeTwitter.apps.notes.models.Note`.
+    **Context**
+    ''Context''
+    ''Notes'' An instance of :model:`LikeTwitter.apps.notes.models.Note`.
+    **Template:**
+        :template: 'notes.html'
+    """
+    def get(self, request, **kwargs):
+        """
+        Response to GET request. Display lists of notes. If request contains certain 'id' note - 
+        display 'body' of note         
+        """
+        notes_list = Note.objects.all()
+        form = NewNoteForm()
+        return render_to_response('notes.html', {'notes':notes_list, 'form': form}, context_instance=RequestContext(request)) 
+    def post(self,request):
+        """ Adding new note with POST request. Validating input data (min 10 symbols)"""
+        notes_list = Note.objects.all()        
         form = NewNoteForm(request.POST)
-        if form.is_valid(): 
+        if form.is_valid():
             form.save()
-    else:
-         form = NewNoteForm()
-    return render_to_response('notes.html', {'notes':notes_list, 'form': form}, context_instance=RequestContext(request)) 
-def search_id(request, id_of_note):
-    t = loader.get_template(r'search_note.html')
-    c = Context({'id_of_note':id_of_note})
-    return HttpResponse(t.render(c))
+        t = loader.get_template('notes.html')
+        c = Context({'notes':notes_list, 'form': form})
+        return render_to_response('notes.html', {'notes':notes_list, 'form': form}, context_instance=RequestContext(request)) 
+
+class NoteByIdView(View):
+    """
+    Display an individual :model:`LikeTwitter.apps.notes.models.Note`.
+    **Context**
+    ''Context''
+    ''Notes'' An instance of :model:`LikeTwitter.apps.notes.models.Note`.
+    **Template**
+    template: 'search_note.html'
+    """
+    def get(self, request, **kwargs):
+        """
+        Response to GET request.
+        Display 'body' of note with 'id' defined by user
+        """        
+        if kwargs['id_of_note'] != None:
+            t = loader.get_template('search_note.html')
+            c = Context({'id_of_note':kwargs['id_of_note']})
+            return HttpResponse(t.render(c))
 
 
 
