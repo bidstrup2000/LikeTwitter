ec3ece675a70c1c73bc463b198a96556d1f00e83
Andrii Tarasenko <bidstrup2000@gmail.com>
2013-08-24 14:47:03 +0300

    t4_added_docstrings

--- LikeTwitter/apps/notes/admin.py
+++ LikeTwitter/apps/notes/admin.py
@@ -3,6 +3,9 @@ from LikeTwitter.apps.notes.models import Note
 from LikeTwitter.apps.notes.forms import NewNoteForm
 
 class NewNoteAdminForm(admin.ModelAdmin):
+    """ 
+    Class which will register new form with custom widget for admin interface 
+    """
     form = NewNoteForm
 
 admin.site.register(Note, NewNoteAdminForm)
--- LikeTwitter/apps/notes/models.py
+++ LikeTwitter/apps/notes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib import admin
 
 class Note(models.Model):
+    """ Class which store notes"""
     id = models.AutoField(primary_key=True)
     body = models.TextField()
     def __unicode__(self):
