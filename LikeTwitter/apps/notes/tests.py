31caa21c14e628d2396f0f1d0b7e3bff3f80d93a
bidstrup2000 <bidstrup2000@gmail.com>
2013-09-12 13:51:42 +0300

    t1_pep8_fixed

--- LikeTwitter/apps/notes/tests.py
+++ LikeTwitter/apps/notes/tests.py
@@ -1,15 +1,17 @@
-from django.core.urlresolvers import reverse 
+from django.core.urlresolvers import reverse
 from django_webtest import WebTest
 from LikeTwitter.apps.notes.models import Note
 
-class My_test_case(WebTest): 
+
+class My_test_case(WebTest):
     """
     Class which inherits from WebTest and do tests of notes application
     """
-    fixtures = ['fixtures\notes.json']
+    fixtures = [r'fixtures\notes.json']
+
     def test_ticket1_create_app_that_shows_list_of_text_notes(self):
-        """ Check web page for presence all note records from db""" 
+        """ Check web page for presence all note records from db"""
         note_list = Note.objects.all()
-        page = self.app.get(reverse('NotesView'))
+        page = self.app.get(reverse('notes_view'))
         for note_string in note_list:
             assert note_string in page
--- LikeTwitter/urls.py
+++ LikeTwitter/urls.py
@@ -2,21 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from LikeTwitter.apps.notes.views import NotesView
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from django.conf.urls.static import static
 
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$',  NotesView.as_view(), name='NotesView'),
+ admin.autodiscover()
+urlpatterns = patterns(
+    '',
+    url(r'^$',  NotesView.as_view(), name='notes_view'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^static/(?P<path>(css|jquery|js|images)/.+)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),     
+    url(r'^static/(?P<path>(css|jquery|js|images)/.+)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^notes/', NotesView.as_view(), name='NotesView'),
+    url(r'^notes/', NotesView.as_view(), name='notes_view'),
 )
-#r'(?:.*?/)?
-#urlpatterns += staticfiles_urlpatterns()
-#if settings.DEBUG:
-#    urlpatterns += patterns('django.contrib.staticfiles.views',
-#        url(r'^static/(?P<path>.*)$', 'serve'),
-#    )
