2c172250e63768f948cc3ba89949399a3111848b
bidstrup2000 <bidstrup2000@gmail.com>
2013-09-24 19:28:48 +0300

    custom_widget_in_admin_corrected

--- LikeTwitter/apps/notes/custom_widgets.py
+++ LikeTwitter/apps/notes/custom_widgets.py
@@ -13,6 +13,7 @@ class TextareaWithAmountOfSymbol(forms.Textarea):
         tpl = Template(u"""
             <textarea cols="40" id="id_body" name="body" rows="10">$body</textarea>
             <h5 id="id_symbol_count" name = "symbols_count"></h5>
+            <script type="text/javascript" language="javascript" src="js/jquery-1.10.2.min.js"></script>
             <script src="js/text_area_with_counter.js"></script>
             """)
         return mark_safe(tpl.substitute(body=value))
