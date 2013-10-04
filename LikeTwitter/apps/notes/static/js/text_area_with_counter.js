b6e5e295057ba5a62c954b286c76687eb4bb1d97
bidstrup2000 <bidstrup2000@gmail.com>
2013-10-04 03:10:01 +0300

    condition_to_submit_button_added

--- LikeTwitter/apps/notes/static/js/text_area_with_counter.js
+++ LikeTwitter/apps/notes/static/js/text_area_with_counter.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
-jQuery("#id_symbol_count").text('Symbols count(min. 10):    ' + jQuery("#id_body").val().length);
-jQuery("#id_body").keyup(function(){
-jQuery("#id_symbol_count").text('Symbols count(min. 10):    ' + jQuery("#id_body").val().length);});
+	jQuery("input[type=Submit]").attr("disabled","disabled")
+	jQuery("#id_symbol_count").text('Symbols count(min. 10):    ' + jQuery("#id_body").val().length);
+	jQuery("#id_body").keyup(function(){
+		jQuery("#id_symbol_count").text('Symbols count(min. 10):    ' + jQuery("#id_body").val().length);
+		if (jQuery("#id_body").val().length > 9) {
+			jQuery("input[type=Submit]").removeAttr("disabled");
+		}
+		else {
+			jQuery("input[type=Submit]").attr("disabled","disabled")
+		}
+	});
 });
